3cb332d7304b1e54b6acce486a96e4d436ec3b1d
exoduz <exo_duz@yahoo.com>
2019-04-11 13:47:33 -0700

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+
+import logo from './images/dote_logo.png';
+
 import './styles/App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				aaa
+			<div className="wrapper">
+				<header>
+					<img src={ logo } alt="Dote Logo" />
+				</header>
+
+				<div className="content">
+				</div>
+
+				<footer>
+
+				</footer>
 			</div>
 		);
 	}
