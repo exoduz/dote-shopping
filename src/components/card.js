ce14b58eae259d383cb20a23a73dc330d2c5736a
exoduz <exo_duz@yahoo.com>
2019-04-11 17:13:51 -0700

    Do discount

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+const discountPercentage = ( price, msrp ) => {
+	if ( ! price || ! msrp ) {
+		return;
+	}
+
+	if ( price === msrp ) {
+		return;
+	}
+
+	let discount = Math.floor( ( price / msrp ) * 100 );
+
+	return <span className="card__discount">{ `${ discount }% off` }</span>;
+}
+
 const Card = ( { data } ) => {
 	return (
 		<div className="card">
@@ -12,7 +26,15 @@ const Card = ( { data } ) => {
 					{ data.store_name && <span className="card__store">{ data.store_name }<br /></span> }
 					{ data.name && <span className="card__name">{ data.name }<br /></span> }
 					{ data.price && <span className="card__price">{ `$${ Number( data.price / 100 ).toFixed(2) }` }</span> }
-					{ data.msrp && <span className="card__msrp">{ `$${ Number( data.msrp / 100 ).toFixed(2) }` }</span> }
+					{ data.msrp
+						&& data.price !== data.msrp
+						&& <span className="card__msrp">{ `$${ Number( data.msrp / 100 ).toFixed(2) }` }</span>
+					}
+					{ data.price
+						&& data.msrp
+						&& data.price !== data.msrp
+						&& discountPercentage( data.price, data.msrp )
+					}
 				</p>
 			</div>
 		</div>
